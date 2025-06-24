26e8cbbd97cc852d63f12f5857d0db38321729d6
hitochix06 <pachara.philibert@hotmail.fr>
2025-06-24 22:30:54 +0200

    feat: addition of the "About" page and update of the links in the navigation bar

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -42,7 +42,7 @@ export function Navbar() {
               Comment ça marche
             </Link>
             <Link
-              href="/a-propos"
+              href="/a-features"
               className="text-gray-700 hover:text-blue-600 font-medium transition-colors"
             >
               À propos
@@ -108,7 +108,7 @@ export function Navbar() {
                 Comment ça marche
               </Link>
               <Link
-                href="/a-propos"
+                href="/a-features"
                 className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-blue-600 hover:bg-gray-50"
                 onClick={() => setMobileMenuOpen(false)}
               >
